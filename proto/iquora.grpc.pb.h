a3972e14c85bad11738daa3940550a2c0de6b64b
Vishal <vishalpraneeth7771@gmail.com>
2025-10-06 21:03:36 +0530

    proto version compatibility

--- proto/iquora.grpc.pb.h
+++ proto/iquora.grpc.pb.h
@@ -24,6 +24,7 @@
 #include <grpcpp/support/status.h>
 #include <grpcpp/support/stub_options.h>
 #include <grpcpp/support/sync_stream.h>
+#include <grpcpp/ports_def.inc>
 
 namespace iquora {
 
@@ -864,4 +865,5 @@ class StateStore final {
 }  // namespace iquora
 
 
+#include <grpcpp/ports_undef.inc>
 #endif  // GRPC_iquora_2eproto__INCLUDED
